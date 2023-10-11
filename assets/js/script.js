e7a8959c43b224400b882d3e8205a364ebf62b02
YURI <cg3026507@LAB202M20.IFSP>
2023-10-11 10:51:16 -0300

    finalizando menu mobile

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const navOpenBtn = document.querySelector('[data-menu-open]');
+const navCloseBtn = document.querySelector('[data-menu-close]');
+const navBar = document.querySelector('[data-navbar]');
+const overlay = document.querySelector('[data-overlay]');
+
+const nav = [navOpenBtn, navCloseBtn, overlay];
+
+for (var i=0; i<nav.length; i++) {
+    nav[i].addEventListener('click', function(){
+    navBar.classList.toggle('active'); //quando clica, aparece. qaundo desativa, deparece.
+})
+}
